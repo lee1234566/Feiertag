82ee2cf77c39a6a0bfabb3f3dadeed791401e81f
lee1234566 <125450242+lee1234566@users.noreply.github.com>
2024-01-11 17:55:53 +0100

    alles

---
+++ @3/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1> guck guck </h1>
+
+
+    <div class="card flex justify-content-center">
+        <Button label="Submit bhhhhhhhhhhhhhhh" />
+    </div>
+
+
+
+
+</template>
+
+<style scoped>
+
+</style>
